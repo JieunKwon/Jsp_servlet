b7755b47de5f4fcf75bf6ad57bc96030cd36b8d2
Jieun (Julia) <39966599+JieunKwon@users.noreply.github.com>
2018-10-31 19:33:26 -0600

    Add files via upload

---
+++ WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form method="POST" action="CarlendarServlet">
+	
+	User Name: <input type="text" name="username" id="username" value="" maxlength="30" class="box_general">
+	<br>
+ 
+	<br><br><input type="submit" value="submit">
+			
+	</form>
+	
+</body>
+</html>
---
+++ src/employees/CarlendarServlet.java
@@ -0,0 +1,54 @@
+package employees;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CarlendarServlet
+ */
+@WebServlet("/CarlendarServlet")
+public class CarlendarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CarlendarServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+	
+		String username = request.getParameter("username");
+		Calendar calendar = new Calendar();
+		calendar.setName(username);
+		request.setAttribute("bean", calendar);
+		
+		RequestDispatcher rd=request.getRequestDispatcher("Calendar.jsp");
+		rd.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
